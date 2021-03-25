f3bb940ee49b177507732b253ee4aaefc50e9d6d
dominikazb <domi.biesiada@gmail.com>
2021-03-25 19:37:48 +0100

    Task 0 - Cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FooterComponent } from './modules/footer/footer.component';
-import { HeaderComponent } from './modules/header/header.component';
-import { AppStartComponent } from './modules/app-start/app-start.component';
-import { MaterialModule } from './shared/modules/material.module';
-import { ElevationDirective } from './shared/directives/elevation.directive';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FooterComponent} from './modules/footer/footer.component';
+import {HeaderComponent} from './modules/header/header.component';
+import {AppStartComponent} from './modules/app-start/app-start.component';
+import {MaterialModule} from './shared/modules/material.module';
+import {ElevationDirective} from './shared/directives/elevation.directive';
 import {ReactiveFormsModule} from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { DatePipe } from '@angular/common';
-import { ProcessVariablesService } from './shared/services/process-variables.service';
-import { QuestionScreensModule } from './modules/question-screens/question-screens.module';
+import {RouterModule} from '@angular/router';
+import {DatePipe} from '@angular/common';
+import {ProcessVariablesService} from './shared/services/process-variables.service';
+import {QuestionScreensModule} from './modules/question-screens/question-screens.module';
 
 
 @NgModule({
--- src/app/modules/app-start/app-start.module.ts
+++ src/app/modules/app-start/app-start.module.ts
@@ -1,8 +1,6 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AppStartComponent } from './component/app-start/app-start.component';
-
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AppStartComponent} from './app-start.component';
 
 @NgModule({
   declarations: [AppStartComponent],
